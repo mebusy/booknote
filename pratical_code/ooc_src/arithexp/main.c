571658a067f0c8c17e81dff4852a97b3e94bd3a9
qibinyi <qibinyi@qibinyis-MacBook-Pro.local>
2019-09-11 21:33:58 +0800

    m

---
+++ pratical_code/ooc_src/arithexp/main.c
@@ -0,0 +1,37 @@
+#include <setjmp.h> 
+#include <stdarg.h>
+
+void error (const char * , ...) ;
+
+static enum tokens token; /*current input symbol*/
+static jmp_buf onError;
+
+int main (void) {
+    volatile int errors = 0;
+    char buf [BUFSIZ];
+
+    if (setjmp(onError)) 
+        ++ errors;
+
+    while (gets(buf)) { 
+        if (scan(buf)) {
+            void * e = sum();
+            if (token)
+                error("trash after sum");
+            process(e); 
+            delete(e);
+        }
+    }
+    return errors > 0;
+
+}
+
+
+void error (const char * fmt, ...) { 
+    va_list ap;
+    va_start(ap, fmt);
+    vfprintf(stderr, fmt, ap), putc(’\n’, stderr); va_end(ap);
+    longjmp(onError, 1);
+}
+
+
