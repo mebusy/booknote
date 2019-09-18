c195b3ebbadd64c3b1195d9a32417700fc43420a
qibinyi <qibinyi@qibinyis-imac.ubisoft.org>
2019-09-18 18:11:36 +0800

    m

--- pratical_code/ooc_src/6_class_hierarchy/Object.h
+++ pratical_code/ooc_src/6_class_hierarchy/Object.h
@@ -1,13 +1,18 @@
-#ifndef	OBJECT_H
-#define	OBJECT_H
+
+#ifndef OBJECT_H
+#define OBJECT_H
 
 #include <stdarg.h>
 #include <stddef.h>
 #include <stdio.h>
+#include "Object.r.h"
 
-extern const void * Object;		/* new(Object); */
+extern const void * Object;     /* new(Object); */
 
 void * new (const void * class, ...);
+// when pass a `const void *` pointer, ( we make new(Class) as  const void * )
+// it will generate an warning message so that
+// compiler will warn us there we are gonna delete a Class 
 void delete (void * self);
 
 const void * classOf (const void * self);
@@ -18,9 +23,9 @@ void * dtor (void * self);
 int differ (const void * self, const void * b);
 int puto (const void * self, FILE * fp);
 
-extern const void * Class;	/* new(Class, "name", super, size
-										sel, meth, ... 0); */
+extern const void * Class;  /* new(Class, "name", super, size
+                                        sel, meth, ... 0); */
 
-const void * super (const void * self);	/* class' superclass */
+const void * super (const void * self); /* class' superclass */
 
 #endif
--- pratical_code/ooc_src/6_class_hierarchy/Object.r.h
+++ pratical_code/ooc_src/6_class_hierarchy/Object.r.h
@@ -1,19 +1,24 @@
 #ifndef OBJECT_R
 #define OBJECT_R
 
+#include <stddef.h>
+#include <stdarg.h>
+#include <stdio.h>
+
+
 struct Object {
-	const struct Class * class;	/* object's description */
+    const struct Class * class; /* object's description */
 };
 
 struct Class {
-	const struct Object _;			/* class' description */
-	const char * name;				/* class' name */
-	const struct Class * super;		/* class' super class */
-	size_t size;					/* class' object's size */
-	void * (* ctor) (void * self, va_list * app);
-	void * (* dtor) (void * self);
-	int (* differ) (const void * self, const void * b);
-	int (* puto) (const void * self, FILE * fp);
+    const struct Object _;          /* class' description */
+    const char * name;              /* class' name */
+    const struct Class * super;     /* class' super class */
+    size_t size;                    /* class' object's size */
+    void * (* ctor) (void * self, va_list * app);
+    void * (* dtor) (void * self);
+    int (* differ) (const void * self, const void * b);
+    int (* puto) (const void * self, FILE * fp);
 };
 
 void * super_ctor (const void * class, void * self, va_list * app);
